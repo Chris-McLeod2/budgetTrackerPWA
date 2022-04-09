8365885b39fb3acb1111bb16e782a158ceb1ee1d
Chris-McLeod2 <91762149+Chris-McLeod2@users.noreply.github.com>
2022-04-09 09:26:38 -0500

    added manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,28 @@
-const WebpackPwaManifest = require("webpack-pwa-manifest");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
+
+// const config = {
+//     plugins: [
+//         new webpack.ProvidePlugin({
+//           $: 'jquery',
+//           jQuery: 'jquery'
+//         }),
+//         new BundleAnalyzerPlugin({
+//           analyzerMode: 'static'
+//         }),
+//     new WebpackPwaManifest({
+//         name: "Budget-Tracker",
+//         short_name: "Budget-Tracker",
+//         description: "An app that allows you to manage your budget.",
+//         start_url: "/index.html",
+//         background_color: "#6666ff",
+//         theme_color: "#ffffff",
+//         fingerprints: false,
+//         inject: false,
+//         icons: [{
+//           src: path.resolve("assets/img/icons/icon-512x512.png"),
+//           sizes: [72, 96, 128, 144, 152, 192, 384, 512],
+//           destination: path.join("assets", "icons")
+//         }]
+//     })
+// ]
+// }
